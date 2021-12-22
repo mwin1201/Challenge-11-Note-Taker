25cc8c950a6d7c5de5259642be681950698448fe
Max W <mwinter@wesleyan.edu>
2021-12-21 21:49:28 -0600

    added feature to add notes

--- routes/apiRoutes/notesRoute.js
+++ routes/apiRoutes/notesRoute.js
@@ -1,12 +1,27 @@
 const router = require("express").Router();
 const notes = require('../../db/db.json');
+const writeFile = require('../../lib/notes');
 
-
+// get saved notes
 router.get('/notes', (req, res) => {
     let response = notes;
     res.json(response);
 });
 
+// post note written by user
+router.post('/notes', ({ body }, res) => {
+    if (!body.title || !body.text) {
+        res.sendStatus(404);
+        return;
+    }
+    let response = notes;
+    response.push(body);
+    writeFile(JSON.stringify(response));
+    res.json({
+        message: 'success',
+        data: body
+    });
+});
 
 
 
