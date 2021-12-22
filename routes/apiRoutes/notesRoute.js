e6db9056457305271b5e6a82f17aa508b7e27a5b
Max W <mwinter@wesleyan.edu>
2021-12-21 19:06:12 -0600

    task notes appearing on page

--- routes/apiRoutes/notesRoute.js
+++ routes/apiRoutes/notesRoute.js
@@ -3,12 +3,8 @@ const notes = require('../../db/db.json');
 
 
 router.get('/notes', (req, res) => {
-    if (notes) {
-        res.json(notes);
-    }
-    else {
-        res.sendStatus(404);
-    }
+    let response = notes;
+    res.json(response);
 });
 
 
